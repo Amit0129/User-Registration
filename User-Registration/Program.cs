965c1be1f188223a91ef44a2836a2372212a953a
Amit0129 <amitkumarnayak40@gmail.com>
2023-01-11 00:04:50 +0530

    [Amit]Validating First Name

---
+++ User-Registration/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User registration Problem statement");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            Console.WriteLine("Enter First name :");
+            string fname = Console.ReadLine();
+            RegexValidation regex = new RegexValidation();
+            regex.CheckFirstName(fname);
+        }
+    }
+}
---
+++ User-Registration/RegexValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    internal class RegexValidation
+    {
+        public void CheckFirstName(string firstname)
+        {
+            string validateFirstName = "^[A-Z][a-z]{3,20}$";
+            try
+            {
+                if (Regex.IsMatch(firstname, validateFirstName))
+                    Console.WriteLine("First Name Valid");
+                else
+                    Console.WriteLine("Invalid name!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
