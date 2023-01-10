0bc8da24ef0b6bf72871d89454214b9795bfa2fb
Amit0129 <amitkumarnayak40@gmail.com>
2023-01-11 00:33:10 +0530

    [Amit]validating password rule 2 Atlest one uppercase

--- User-Registration/RegexValidation.cs
+++ User-Registration/RegexValidation.cs
@@ -56,7 +56,7 @@ namespace User_Registration
         }
         internal void CheckPassword(string password)
         {
-            string validatepassword = "^[A-Za-z0-9]{8,20}$";
+            string validatepassword = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
             try
             {
                 if (Regex.IsMatch(password, validatepassword))
